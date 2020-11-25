032beee20223c6cadbc61e7f97f26a3eeadfa770
LukaszPC <galczo230@gmail.com>
2020-11-25 13:37:12 +0100

    Fused location fix

--- appka/app/src/main/java/com/example/bicycletracker/view/LastLocationActivity.kt
+++ appka/app/src/main/java/com/example/bicycletracker/view/LastLocationActivity.kt
@@ -1,6 +1,7 @@
-package com.example.bicycletracker.view.interfaces
+package com.example.bicycletracker.view
 
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.*
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,14 +10,16 @@ import com.example.bicycletracker.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.android.synthetic.main.special_places_layout.*
 
-
-class LastLocationActivity : AppCompatActivity() {
+class LastLocationActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.special_places_layout)
-        lokalizacja.setOnClickListener{getLastLocation()}
 
+        lokalizacja.setOnClickListener {
+            lokalizacja.text = "Gps Loaded"
+            getLastLocation()
+        }
     }
 
     private fun getLastLocation() {
@@ -24,11 +27,11 @@ class LastLocationActivity : AppCompatActivity() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
+            ) != PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+            ) != PERMISSION_GRANTED
         ) {
             println("odmowa")
             return
@@ -36,14 +39,12 @@ class LastLocationActivity : AppCompatActivity() {
         val getLastLocationTask = FusedLocationProviderClient(this).lastLocation
 
         getLastLocationTask.addOnSuccessListener {
-            Toast.makeText(this, "Twoja lokalizacja to: $it", Toast.LENGTH_SHORT).show()
             myLocation.text = it.toString()
-            println("success: " + it)
         }
         getLastLocationTask.addOnFailureListener {
             Toast.makeText(this, "Nie określono lokalizacji gps", Toast.LENGTH_SHORT).show()
             println("error: " + it)
         }
     }
-}
 
+}
--- appka/app/src/main/java/com/example/bicycletracker/view/MenuActivity.kt
+++ appka/app/src/main/java/com/example/bicycletracker/view/MenuActivity.kt
@@ -1,21 +1,12 @@
 package com.example.bicycletracker.view
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.example.bicycletracker.R
 import com.example.bicycletracker.tools.loadAppBackground
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.menu_layout.*
-import java.util.jar.Manifest
 
 class MenuActivity : AppCompatActivity() {
 
@@ -38,8 +29,8 @@ class MenuActivity : AppCompatActivity() {
                 intent = Intent(this, FindUsActivity::class.java)
             }
             R.id.special_places -> {
-                intent = Intent(this, SpecialPlacesActivity::class.java)
-
+               // intent = Intent(this, SpecialPlacesActivity::class.java)
+                intent = Intent(this, LastLocationActivity::class.java)
             }
             R.id.settings -> {
                 intent = Intent(this, SettingsActivity::class.java)
