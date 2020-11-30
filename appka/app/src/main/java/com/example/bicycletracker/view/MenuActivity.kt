0f6fb613fb291ae0755c9fb1ef6ed208295d5d40
LukaszPC <galczo230@gmail.com>
2020-11-30 23:02:15 +0100

    tracker

--- appka/app/src/main/java/com/example/bicycletracker/view/MenuActivity.kt
+++ appka/app/src/main/java/com/example/bicycletracker/view/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
@@ -36,7 +37,8 @@ class MenuActivity : AppCompatActivity() {
                 intent = Intent(this, FindUsActivity::class.java)
             }
             R.id.special_places -> {
-                intent = Intent(this, SpecialPlacesActivity::class.java)
+                //intent = Intent(this, SpecialPlacesActivity::class.java)
+                showWaypoints()
             }
             R.id.settings -> {
                 intent = Intent(this, SettingsActivity::class.java)
@@ -45,6 +47,14 @@ class MenuActivity : AppCompatActivity() {
                 intent = Intent(this, LastLocationActivity::class.java)
             }
         }
+       // startActivity(intent)
+    }
+
+    private fun showWaypoints(){
+        val url = "https://www.google.com/maps/dir/?api=1&origin=54.1194055,15.2053655&destination=54.0954555,15.0828297&waypoints=54.0942191,15.1077139%7C54.0966391,15.0977442&travelmode=bicycling"
+        intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
         startActivity(intent)
     }
 
